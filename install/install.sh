d3cf83a08151d2508b0147c40c152e3c5aed197f
Piotr Stawirej <piotr.stawirej@gmail.com>
2021-04-15 22:27:45 +0200

    chore: Fix typo

--- install/install.sh
+++ install/install.sh
@@ -2,13 +2,13 @@
 
 set -eou pipefail
 
-echo "Download bc ..."
+echo "Download bs ..."
 curl --location --progress-bar "https://raw.githubusercontent.com/stawirej/git-branch-select/master/src/bs" > bs
 
 echo "Add execution rights ..."
 chmod ugo+x ./bs
 
-echo "Install bc in /usr/local/bin ..."
+echo "Install bs in /usr/local/bin ..."
 mv ./bs /usr/local/bin
 
 echo "Done :)"
